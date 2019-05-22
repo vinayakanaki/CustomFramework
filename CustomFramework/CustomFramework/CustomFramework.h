41bad5ea1414051791c48b4e1c5f8d124902be70
Pravin Gupta <Credentek@Mac-Mini.local>
2019-05-22 12:10:24 +0530

    This is my first custom pod

---
+++ CustomFramework/CustomFramework/CustomFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Pravin Gupta on 5/22/19.
+//  Copyright © 2019 Vinayak Kanaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
---
+++ CustomFramework/CustomFramework/Service.swift
@@ -0,0 +1,25 @@
+//
+//  Service.swift
+//  CustomFramework
+//
+//  Created by Pravin Gupta on 5/22/19.
+//  Copyright © 2019 Vinayak Kanaki. All rights reserved.
+//
+
+import Foundation
+
+public class Service
+{
+    private init()
+    {
+        
+    }
+    
+    public static func doSomething() -> String
+    {
+        return "This Vinayak Kanaki iOS Cocoa Custom Pos."
+    }
+    
+    
+    
+}
